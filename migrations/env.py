57fc135c95487eddd11bb03ae32df5807c376aae
MrGAN12009 <mrgan12009@gmail.com>
2025-12-05 13:43:49 +0300

    Disable fileConfig in migrations/env.py to prevent issues with missing alembic.ini in Docker.

--- migrations/env.py
+++ migrations/env.py
@@ -24,8 +24,9 @@ config.set_main_option('sqlalchemy.url', Config.SQLALCHEMY_DATABASE_URI)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# Отключаем fileConfig так как alembic.ini не копируется в Docker
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
